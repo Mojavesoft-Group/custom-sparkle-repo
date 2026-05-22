c2dd8edc5288054f1bd9eb30de1f312fcbec4d5e
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-05-22 13:41:15 -0400

    Update example.js

--- mods/example.js
+++ mods/example.js
@@ -1,4 +1,4 @@
-return class extends Mod {
+genreturn class extends Mod {
     // Metadata 
     ID = "example-mod"; // the id of the mod
     NAME = "Example Mod"; // human-readable name
@@ -8,60 +8,6 @@ return class extends Mod {
     DEPENDS = []; // dependencies (mod ids, useful for libraries)
     DO_MENU = true; // whether to add a menu item
     OPTIONS_FORMAT = [
-        "HEADING 1", // Headings
-        {
-            id: "number",
-            name: "A number",
-            type: "number",
-            default: 3,
-            // optional
-            min: 1,
-        },
-        {
-            id: "slider",
-            name: "A number slider",
-            type: "number",
-            default: 3,
-            // optional, both min and max will make the input into a slider
-            min: 1,
-            max: 4,
-            resolution: 0.1
-        },
-        null, // spacer
-        {
-            id: "helloTest",
-            name: "Hello String",
-            type: "string",
-            default: "Hello!",
-        },
-        {
-            id: "dropDown",
-            name: "Hi Dropdown",
-            type: "string",
-            default: "hello",
-            menu: {
-                "Hello": "hello",
-                "Example": "example",
-                "Sparkle": "sparkle",
-            },
-            readOnly: true,
-        },
-        {
-            id: "color",
-            name: "A color",
-            type: "color",
-            default: new Color(255, 0, 128),
-        },
-        {
-            id: "everyHello",
-            name: "All Category Hellos",
-            type: "string",
-            // numbers and strings can have arrays as defaults
-            default: ["hi", "hello", "sick", "howdy"],
-            // optional, for arrays
-            minLength: 1,
-            maxLength: 5,
-        },
     ]; // format for options
 
     // Main function - gets ran when the mod is loaded
